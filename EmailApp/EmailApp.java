0c51f56872e2e4e21ee690936075938db99a82ae
talwinder777 <stalwinder777@gmail.com>
2019-09-29 16:41:32 -0500

    Add files via upload

---
+++ EmailApp/EmailApp.java
@@ -0,0 +1,12 @@
+package emailApp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Email em1 = new Email("Sandeep","Singh");
+		
+		System.out.println(em1.showInfo());
+	}
+
+}
